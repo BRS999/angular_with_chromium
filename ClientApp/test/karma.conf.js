7d9548ed313167ddfb266038dd566320aba04f9d
Ben <ben.richard.spencer@outlook.com>
2017-10-23 16:27:40 -0400

    karma

--- ClientApp/test/karma.conf.js
+++ ClientApp/test/karma.conf.js
@@ -1,6 +1,8 @@
 // Karma configuration file, see link for more information
 // https://karma-runner.github.io/0.13/config/configuration-file.html
 
+process.env.CHROME_BIN = require("puppeteer").executablePath();
+
 module.exports = function (config) {
     config.set({
         basePath: '.',
@@ -17,7 +19,7 @@ module.exports = function (config) {
         colors: true,
         logLevel: config.LOG_INFO,
         autoWatch: true,
-        browsers: ['Chrome'],
+        browsers: ['ChromeHeadless'],
         mime: { 'application/javascript': ['ts','tsx'] },
         singleRun: false,
         webpack: require('../../webpack.config.js')().filter(config => config.target !== 'node'), // Test against client bundle, because tests run in a browser

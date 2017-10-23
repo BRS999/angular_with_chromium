2eb4e736900b9fa9805bfa0746d504fd934abe64
Ben <ben.richard.spencer@outlook.com>
2017-10-23 16:29:48 -0400

    single run

--- ClientApp/test/karma.conf.js
+++ ClientApp/test/karma.conf.js
@@ -21,7 +21,7 @@ module.exports = function (config) {
         autoWatch: true,
         browsers: ['ChromeHeadless'],
         mime: { 'application/javascript': ['ts','tsx'] },
-        singleRun: false,
+        singleRun: true,
         webpack: require('../../webpack.config.js')().filter(config => config.target !== 'node'), // Test against client bundle, because tests run in a browser
         webpackMiddleware: { stats: 'errors-only' }
     });
